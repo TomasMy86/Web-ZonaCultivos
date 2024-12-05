4c30fd3a421edfe86ffb2ca46a9d3b11bae84e6d
Tomás My <89661516+TomasMy86@users.noreply.github.com>
2024-12-05 18:48:38 -0300

    Add Files

---
+++ Web ZonaCultivos/templates/page_content.php
@@ -0,0 +1,7 @@
+<?php 
+    include('inicio.php');
+    include('quienes_somos.php');
+    include('nuestros_productos.php');
+    include('colocacion_y_cuidados.php');
+    include('contacto.php');
+?>
